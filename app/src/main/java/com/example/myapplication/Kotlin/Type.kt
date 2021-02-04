63e0c72c7b0d2bfbaadf0aab01573958d87308bc
github username <chang.lim92@gmail.com>
2021-02-04 22:09:04 +0900

    Kotlin Type & Variable

---
+++ app/src/main/java/com/example/myapplication/Kotlin/Type.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication.Kotlin
+
+//정수형: Byte < Short < Int < Long   크기 순서
+//실수형 (decimal): Float < Double
+//문자 char
+//문자열 String
+//Boolean
+
+
+// 변수 선언 2가지 방법
+var number1 : Int = 20
+var number2 = 20
+
+var hello1 : String = "hello"
+var hello2 = "hello"
+
+var point1 : Double = 3.4
+var point2 = 3.4
+
---
+++ app/src/main/java/com/example/myapplication/Kotlin/Variable.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication.Kotlin
+
+// Var vs Val
+// Var --> 마음대로 바꿀 수 있는 변수
+// Val --> 바꿀 수 없는 변수
+
+var num = 10
+var hello = "hello"
+val point = 3.4
+
+fun main(args:Array<String>) {
+    println(num)
+    println(hello)
+    println(point)
+
+}
